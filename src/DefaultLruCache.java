0686bb14b317c9ea2b76cebc699703f48953b7a1
Michal Trojanowski <mtroja98@gmail.com>
2024-05-24 20:47:41 +0200

    2

--- src/DefaultLruCache.java
+++ src/DefaultLruCache.java
@@ -3,7 +3,19 @@ import java.util.LinkedHashMap;
 public class DefaultLruCache extends LinkedHashMap<Integer, Integer>
 							implements LruCache {
 
-    <write your code here>
+    private Map<Integer, Integer> map = new LinkedHashMap<>();
+
+
+
+    int get(int key) {
+        for (Integer mapKey : map.setKeys()) {
+            if (key == mapKey ) {
+                return mapKey.valueOf();
+            }
+        }
+
+
+    }
 
 
 }
